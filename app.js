0e79e5b8392fc19df74c5ea754a92c83feb722b5
= <=>
2024-03-27 16:19:03 +0000

    Feat: First API call and Parsing json data

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const app = express();
 require('dotenv/config');
 const api = process.env.API_URL;
 
-app.get(api + '/products', (req, res) => {
-    res.send('Hello World !');
+// Middleware to understand response JSON
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 
 app.listen(9090, () => {
