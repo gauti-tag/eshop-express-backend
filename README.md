06ac5cc214e6e2356778785a07979292aaa4d144
= <=>
2024-07-21 11:08:08 +0000

    Refactor: application file and docs

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 ### Create New Project
 `npm init`
 
-### Install `nodemon` server to the project
+### Install `nodemon` nodejs server to the project
 `npm install nodemon`
 
 ### Configurations
 Add `"start": "nodemon app.js"` to `"scripts"` in the package.json
 
-### Install express
+### Install express [ Backend node js ]
 `npm install express`
 
 
-### Install Plugin dotenv
+### Install Plugin dotenv to handle variables in env file
 `npm install dotenv`
 
 ### Install Plugin morgan used to logging API Request
@@ -19,4 +19,4 @@ Add `"start": "nodemon app.js"` to `"scripts"` in the package.json
 
 
 ### Install Plugin mongoose used to use and connect Mongo DataBase
-`npm install morgan`
+`npm install mongoose`
--- app.js
+++ app.js
@@ -1,39 +1,46 @@
 const express = require('express');
 const app = express();
 
+// Require to use variable in env file
 require('dotenv/config');
 
 // Get value of API_URL environment variable
 const api = process.env.API_URL;
 
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+// Products routers
+const productsRouter = require('./routers/products');
+
+const morgan = require('morgan'); // Library for writing log
+const mongoose = require('mongoose'); // library for MangoDB ( Oriented Collections )
 
 // Middleware to understand response JSON
 
-// Use to render proper json 
+// Use to render proper json
 app.use(express.json());
 
 // Use to write log in the App
 app.use(morgan('tiny'));
 
+// Routers
+app.use(`${api}/products`, productsRouter);
+
 // Define the schema for the product
-const productSchema = mongoose.Schema({
+/*const productSchema = mongoose.Schema({
     name: String,
     image: String,
     countInStock: {
         type: Number,
         required: true
     }
-});
+});*/
 
 // Define model for product and start with capital letter
-const Product = mongoose.model('Product', productSchema);
+//const Product = mongoose.model('Product', productSchema);
 
 // Connect to MongoDB DataBase
-mongoose.connect('mongodb+srv://gauti:aKHf3FKrcPwb7BvX2024@cluster0.pwes2bh.mongodb.net/eshop-database?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
     .then(() => {
-        // Display promise for success connection 
+        // Display promise for success connection
         console.log('Database Connection is ready ...');
     })
     .catch((err) => {
@@ -41,53 +48,8 @@ mongoose.connect('mongodb+srv://gauti:aKHf3FKrcPwb7BvX2024@cluster0.pwes2bh.mong
         console.log(err);
     })
 
-// API Get all products (async) / (await)
-app.get(`${api}/products`, async (req, res) => {
-
-    const productList = await Product.find();
-
-    if (!productList) {
-        res.status(400).json({
-            description: 'No Product in the database'
-        })
-    }
-
-    res.send(productList);
-
-    /*const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
-    }
-    res.send(product);*/
-
-})
-
-// API Post product
-app.post(`${api}/products`, (req, res) => {
-
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createProduct => {
-        res.status(201).json(createProduct)
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            status: 500
-        })
-    })
-
-    //const newProduct = req.body;
-    //console.log(newProduct);
-    //res.send(newProduct);
-})
-
 // Open running Server on a port
 app.listen(9090, () => {
     console.log(api);
-    console.log('App is running');
+    console.log('App is running on http://localhost:9090');
 })
