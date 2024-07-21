2fccf72d8cebd8911f4f4051a699e3ce530282ca
= <=>
2024-07-21 13:33:30 +0000

    Refactor: Category post params

--- routers/categories.js
+++ routers/categories.js
@@ -1,3 +1,4 @@
+const { status } = require('express/lib/response');
 const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
@@ -14,20 +15,18 @@ router.get('/', async (req, res) => {
 })
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const category = new Category({
-        name: req.body.name
+        name: req.body.name,
+        color: req.body.color,
+        icon: req.body.icon
     })
 
-    category.save().then((createCategory) => {
-        res.status(201).json(createCategory);
-    }).catch((e) => {
-        res.status(500).json({
-            status: 500,
-            error: e
-        })
-    })
+    const createCategory = await category.save();
+    if (!createCategory)
+        return res.status(400).json({ status: 400, description: 'The category can not be created' });
 
+    res.status(200).json(createCategory);
 
 })
 
