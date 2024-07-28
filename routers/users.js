6f1aa4616bb9178ae4c601ba33d3564cac9436f0
= <=>
2024-07-28 10:48:13 +0000

    Refactor: Post / Register a new user [Rest API]

--- routers/users.js
+++ routers/users.js
@@ -3,25 +3,34 @@ const express = require('express');
 const router = express.Router();
 
 // User Get
-router.get('/', async (req, res) => {
+router.get(`/`, async (req, res) => {
     const userList = await User.find();
     if (!userList) {
         res.status(400).json({
-            description: 'No User in the database'
-        })
+            status: 400,
+            message: 'No User in the database'
+        });
     }
-    res.send(userList)
+    res.status(200).json({ status: 200, message: 'List of users', data: userList });
 });
 
 // User Post
-router.post('/', (req, res) => {
+router.post(`/`, (req, res) => {
     const user = new User({
         name: req.body.name,
-        email: req.body.email
+        email: req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
     })
 
     user.save().then((createUser) => {
-        res.status(201).json(createUser);
+        res.status(201).json({ status: 201, message: 'created user', data: createUser });
     }).catch((e) => {
         res.status(500).json({
             status: 500,
